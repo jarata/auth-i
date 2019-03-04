d32720f514e778c8cf6bd6c30fe200d79f0aa7ca
J A <jarata@gmail.com>
2019-03-04 15:31:27 -0800

    /api/login POST

--- server.js
+++ server.js
@@ -23,22 +23,46 @@ server.post('/api/register', async (req, res) => {
     const hash = bcrypt.hashSync(user.password, 12);
     user.password = hash;
     try {
-        let newUser = await db('users').insert(user)
+        let newUser = await db('users').insert(user);
         res.status(201).json(newUser)
     } catch (e) {
         console.log(e)
         res.status(500).json({
-            error: "Error"
+            error: "Error 500", e
         })
     }
 });
 
 server.post('/api/login', async (req, res) => {
-    res.send('Login POST')
+    // res.send('Login POST')
+    let {username, password} = req.body;
+    try {
+        let user = await db('users').where({username}).first();
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({
+                message: 'Welcome'
+            })
+        } else {
+            res.status(401).json({
+                message: 'Invalid credentials'
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: 'Error 500', e
+        })
+    }
 });
 
 server.get('/api/users', async (req, res) => {
-    res.send('Users GET')
+    try {
+        users = await db('users').select('id', 'username', 'password');
+        res.status(200).json(users)
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e)
+    }
 });
 
 module.exports = server;
