7676401b4e28f5fa42e1c1223c8cdbae8443f3be
J A <jarata@gmail.com>
2019-03-04 14:50:54 -0800

    /api/register POST

--- server.js
+++ server.js
@@ -12,21 +12,33 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send("Test");
 });
 
 server.post('/api/register', async (req, res) => {
-    res.send('Register POST')
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+    try {
+        let newUser = await db('users').insert(user)
+        res.status(201).json(newUser)
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            error: "Error"
+        })
+    }
 });
 
 server.post('/api/login', async (req, res) => {
     res.send('Login POST')
-})
+});
 
 server.get('/api/users', async (req, res) => {
     res.send('Users GET')
-})
+});
 
 module.exports = server;
